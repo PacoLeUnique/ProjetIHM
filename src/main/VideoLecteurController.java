66378e187199a0823aadbfe422b7c6a4f2942258
PACO <mougeot.axel@orange.fr>
2022-04-13 16:35:17 +0200

    refonte des boutons, ajout du volume

--- src/main/VideoLecteurController.java
+++ src/main/VideoLecteurController.java
@@ -9,8 +9,10 @@ import java.lang.Thread;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -24,25 +26,49 @@ public class VideoLecteurController implements Initializable {
 	@FXML private Button playButton, pauseButton, stopButton;
 	@FXML private ProgressBar progressBar;
 	@FXML private Text progressTimer, videoLengthTimer, slidingProgressTimer;
+	@FXML private Slider volumeBar;
 	
 	private Timer timer = new Timer();
 	private MediaPlayer mediaPlayer;
 	private File file;
 	private Media media;
 	private Duration videoLength;
+	private boolean isMouseOnProgressBar;
 	
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		file = new File("videos/test.mp4");
+		file = new File("videos/logobi.mp4");
 		media = new Media(file.toURI().toString());
 		mediaPlayer = new MediaPlayer(media);
 		mediaView.setMediaPlayer(mediaPlayer);
+		isMouseOnProgressBar = false;
 		
-		// On patiente 1 demi-seconde, le temps de laisser tout le fichier FXML s'initialiser correctement
-		try { Thread.sleep(500); } catch (InterruptedException e1) {	}
+		
+		// On patiente 2,5 secondes, le temps de laisser tout le fichier FXML s'initialiser correctement
+		try { Thread.sleep(2500); } catch (InterruptedException e1) {	}
 		
 		progressBar.setOnMouseMoved(e -> mouseOnProgressBarMoved(progressBar, e));
 		progressBar.setOnMouseEntered(e -> mouseOnProgressBarEntered(progressBar, e));
 		progressBar.setOnMouseExited(e -> mouseOnProgressBarExited(progressBar, e));
+		progressBar.setOnMouseClicked(e -> mouseOnProgressBarClicked(progressBar, e));
+		
+		mediaView.setOnMouseClicked(e -> {
+			
+			switch(mediaPlayer.getStatus()) {
+			case STOPPED:
+				mediaPlayer.play();
+				break;
+			case PAUSED:
+				mediaPlayer.play();
+				break;
+			case PLAYING:
+				mediaPlayer.pause();
+				break;
+				
+			default:
+				mediaPlayer.pause();
+				break;
+			}
+		});
 		
 		mediaPlayer.setOnReady(new Runnable() {
 			@Override
@@ -60,8 +86,10 @@ public class VideoLecteurController implements Initializable {
 				
 				System.out.println("1 DEMI SECONDE, timecode : " + timecode);
 				double ratio = timecode.toMinutes()/videoLength.toMinutes();
-				progressBar.setProgress(ratio);
-				
+				if(!isMouseOnProgressBar) {
+					progressBar.setProgress(ratio);
+				}
+					
 				progressTimer.setText(toTimecode(timecode));
 				
 			}
@@ -69,12 +97,32 @@ public class VideoLecteurController implements Initializable {
 		
 	}
 	
+	private void mouseOnProgressBarClicked(ProgressBar pb, MouseEvent e) {
+		// TODO enculé de ta race
+		// Faut que quand ça clique ça aille au bon timecode
+		double ratio = getMouseRatioOnProgressBar(pb, e);
+		double d = videoLength.toMinutes()*ratio;
+		Duration new_timecode = Duration.minutes(d);
+		
+		mediaPlayer.seek(new_timecode);
+		System.out.println("CLICK");
+	}
+
 	private void mouseOnProgressBarEntered(ProgressBar pb, MouseEvent e) {
 		slidingProgressTimer.setVisible(true);
+		isMouseOnProgressBar = true;
 	}
 	
 	private void mouseOnProgressBarExited(ProgressBar pb, MouseEvent e) {
 		slidingProgressTimer.setVisible(false);
+		
+		Duration timecode = mediaPlayer.getCurrentTime();
+		
+		System.out.println("ON SORT");
+		double ratio = timecode.toMinutes()/videoLength.toMinutes();
+		progressBar.setProgress(ratio);
+		
+		isMouseOnProgressBar = false;
 	}
 
 	private void mouseOnProgressBarMoved(ProgressBar pb, MouseEvent e) {
@@ -89,6 +137,7 @@ public class VideoLecteurController implements Initializable {
 		
 		slidingProgressTimer.setLayoutX(xpos);
 		slidingProgressTimer.setText(timecode);
+		progressBar.setProgress(ratio);
 		return;
 	}
 
@@ -106,6 +155,11 @@ public class VideoLecteurController implements Initializable {
 	public void stopVideo() {
 		mediaPlayer.stop();
 		progressBar.setProgress(0);
+	}
+	
+	@FXML
+	public void changeVolume() {
+		mediaPlayer.setVolume(volumeBar.getValue()/100);
 	}
 	
 	private String toTimecode(Duration d) {
