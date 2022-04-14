2c24033e5cf784a82a1f4259b3aa4868f3d6a907
Touptwi <monsinjon.samuel@gmail.com>
2022-04-14 23:14:23 +0200

    Adjust Video

--- src/main/Video.java
+++ src/main/Video.java
@@ -7,45 +7,24 @@ import java.io.IOException;
 
 public class Video {
 
-    private static String pathname;
+    private final String pathname;
     private String titre;
-    private String categorie;
     private BufferedImage miniature;
 
     /**
-     * Créer un Objet Video
+     * Créer un Objet Video avec un BlackScreen en miniature par default
      * @param Pathname Chemin vers la video
-     * @param Categorie Categorie de la video | "none" par default (facultatif)
      * @param Titre titre voulu de la vidéo | le titre du pathname par default (facultatif)
-     * @param Miniature Image chargé pour prévisualiser la video | un BlackScreen par default (facultatif)
      */
-    public Video(String Pathname,String Categorie, String Titre, BufferedImage Miniature) {
+    public Video(String Pathname, String Titre) {
         this.pathname = Pathname;
-        this.categorie = Categorie;
         this.titre = Titre;
-        this.miniature = Miniature;
-    }
-
-    public Video(String Pathname,String Categorie, String Titre) {
-        this.pathname = Pathname;
-        this.categorie = Categorie;
-        this.titre = Titre;
-        try { this.miniature = ImageIO.read(new File("BlackScreen.png"));}
-        catch (IOException E) { E.printStackTrace(); }
-    }
-
-    public Video(String Pathname, String Categorie) {
-        this.pathname = Pathname;
-        this.categorie = Categorie;
-        String[] pathname_ = Pathname.split("/");
-        this.titre = pathname_[pathname_.length-1];
         try { this.miniature = ImageIO.read(new File("BlackScreen.png"));}
         catch (IOException E) { E.printStackTrace(); }
     }
 
     public Video(String Pathname) {
         this.pathname = Pathname;
-        this.categorie = "none";
         String[] pathname_ = Pathname.split("/");
         this.titre = pathname_[pathname_.length-1];
         try { this.miniature = ImageIO.read(new File("BlackScreen.png"));}
@@ -56,11 +35,9 @@ public class Video {
     public String getPathname() { return pathname; }
     /** Getter du titre */
     public String getTitre() { return titre; }
-    /** Getter de la categorie (none si dans aucune categorie) */
-    public String getCategorie() { return categorie; }
 
-    /** Change le Titre de la video */
+    /** Change le titre de la video */
     public void changeTitre(String nouveauTitre) { this.titre=nouveauTitre; }
-    /** Change la categorie de la video */
-    public void changeCategorie(String Categorie) { this.categorie=Categorie; }
+    /** Change la miniature de la video */
+    public void changeMiniature(BufferedImage nouvelleMiniature) { this.miniature=nouvelleMiniature; }
 }
