fa14b665f31f6b97c022b33fa3cf88b4c19332d8
Touptwi <monsinjon.samuel@gmail.com>
2022-04-14 21:20:11 +0200

    Add miniature to Video

--- src/main/Video.java
+++ src/main/Video.java
@@ -1,19 +1,37 @@
 package main;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Video {
 
-    private String pathname, titre, categorie;
+    private static String pathname;
+    private String titre;
+    private String categorie;
+    private BufferedImage miniature;
 
     /**
      * Créer un Objet Video
      * @param Pathname Chemin vers la video
-     * @param Categorie Categorie de la video (facultatif)
-     * @param Titre titre voulu de la vidéo (facultatif)
+     * @param Categorie Categorie de la video | "none" par default (facultatif)
+     * @param Titre titre voulu de la vidéo | le titre du pathname par default (facultatif)
+     * @param Miniature Image chargé pour prévisualiser la video | un BlackScreen par default (facultatif)
      */
+    public Video(String Pathname,String Categorie, String Titre, BufferedImage Miniature) {
+        this.pathname = Pathname;
+        this.categorie = Categorie;
+        this.titre = Titre;
+        this.miniature = Miniature;
+    }
+
     public Video(String Pathname,String Categorie, String Titre) {
         this.pathname = Pathname;
         this.categorie = Categorie;
         this.titre = Titre;
+        try { this.miniature = ImageIO.read(new File("BlackScreen.png"));}
+        catch (IOException E) { E.printStackTrace(); }
     }
 
     public Video(String Pathname, String Categorie) {
@@ -21,6 +39,8 @@ public class Video {
         this.categorie = Categorie;
         String[] pathname_ = Pathname.split("/");
         this.titre = pathname_[pathname_.length-1];
+        try { this.miniature = ImageIO.read(new File("BlackScreen.png"));}
+        catch (IOException E) { E.printStackTrace(); }
     }
 
     public Video(String Pathname) {
@@ -28,6 +48,8 @@ public class Video {
         this.categorie = "none";
         String[] pathname_ = Pathname.split("/");
         this.titre = pathname_[pathname_.length-1];
+        try { this.miniature = ImageIO.read(new File("BlackScreen.png"));}
+        catch (IOException E) { E.printStackTrace(); }
     }
 
     /** Getter du chemin */
