774d38098b9306c753f0c66dcbf88cfda2cbc579
Touptwi <monsinjon.samuel@gmail.com>
2022-04-15 03:22:07 +0200

    Adjust FXML

--- src/main/Controllers/accueilController.java
+++ src/main/Controllers/accueilController.java
@@ -1,4 +1,30 @@
 package main.Controllers;
 
+import javafx.scene.input.MouseEvent;
+
 public class accueilController {
+
+    public void goto_login1(MouseEvent mouseEvent) {
+    }
+
+    public void goto_login2(MouseEvent mouseEvent) {
+    }
+
+    public void goto_login3(MouseEvent mouseEvent) {
+    }
+
+    public void goto_login4(MouseEvent mouseEvent) {
+    }
+
+    public void goto_login5(MouseEvent mouseEvent) {
+    }
+
+    public void goto_login6(MouseEvent mouseEvent) {
+    }
+
+    public void goto_login7(MouseEvent mouseEvent) {
+    }
+
+    public void goto_login8(MouseEvent mouseEvent) {
+    }
 }
--- src/main/Controllers/loginController.java
+++ src/main/Controllers/loginController.java
@@ -1,4 +1,12 @@
 package main.Controllers;
 
+import javafx.event.ActionEvent;
+
 public class loginController {
+
+    public void login(ActionEvent actionEvent) {
+    }
+
+    public void goto_accueil(ActionEvent actionEvent) {
+    }
 }
